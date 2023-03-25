e5b5bf554440eddbba34e685f33386f787c74ada
Olavo Mello <olavo@agenciadix.com.br>
2023-03-25 16:49:44 -0400

    Move FIPE table to enviroment variables

--- controllers/fipe.js
+++ controllers/fipe.js
@@ -3,7 +3,7 @@ const DB = require('../components/db');
 
 // Configs
 const URL_BASE = "https://veiculos.fipe.org.br/api/veiculos/";
-const dataTable = 295; // Março/2023
+const dataTable = process.env.FIPE_TABLE || 295; // Março/2023;
 const dataTableUpdate = new Date("2023-03");
 const cacheEnabled = Boolean( process.env.CACHE_ENABLED === "true" ) || false;
 const DEBUG = Boolean(process.env.DEBUG === "true" || false ) || false;
